7de1af4f4ab91bee03d92bfbb7d0e6f8828f6f0b
alumnog <alumnog@S923_25.itson.edu.mx>
2023-09-06 11:36:39 -0700

    Versión inicial del proyecto

---
+++ src/banana/Operacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banana;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Operacion {
+    
+ public String[] separar (String oracion){
+String[] resultado = oracion.split(",");
+return resultado;
+}}
